870e355b7bffa83a0f21b90b609fa7d4fae7443e
Wooyoung Myung <mwy3055@gmail.com>
2022-05-06 21:45:50 +0900

    feature: 기본적인 급식 정보 parser 완성 (primitive type만 사용)
    coverage: 95%

---
+++ app/src/main/java/com/practice/hanbitlunch/api/meal/MealDeserializer.kt
@@ -0,0 +1,86 @@
+package com.practice.hanbitlunch.api.meal
+
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import java.lang.reflect.Type
+
+class MealDeserializer : JsonDeserializer<MealResponse> {
+    override fun deserialize(
+        json: JsonElement?,
+        typeOfT: Type?,
+        context: JsonDeserializationContext?
+    ): MealResponse {
+        val response = json?.asJsonObject?.get("mealServiceDietInfo")?.asJsonArray
+            ?: throw MealDeserializerException("Json is $json")
+
+        val headerObject = response[0].asJsonObject
+        val header = parseHeader(headerObject)
+
+        val rowObject = response[1].asJsonObject
+        val row = parseMeals(rowObject)
+
+        return MealResponse(
+            header = header,
+            mealData = row
+        )
+    }
+
+    fun parseHeader(headJson: JsonObject): MealHeader {
+        val heads = headJson.get("head").asJsonArray
+
+        val listTotalCount = heads[0].getAsJsonObject("list_total_count").asInt
+
+        val resultObj = heads[1].getAsJsonObject("RESULT").asJsonObject
+        val resultCode = resultObj.get("CODE").asString
+        val resultMessage = resultObj.get("MESSAGE").asString
+
+        return MealHeader(
+            listTotalCount = listTotalCount,
+            resultCode = MealResultCode(
+                code = resultCode,
+                message = resultMessage
+            )
+        )
+    }
+
+    fun parseMeals(rowJson: JsonObject): List<Meal> {
+        val rows = rowJson.get("row").asJsonArray
+        return rows.map { row ->
+            with(row.asJsonObject) {
+                Meal(
+                    officeCode = get("ATPT_OFCDC_SC_CODE").asString,
+                    officeName = get("ATPT_OFCDC_SC_NM").asString,
+                    schoolCode = get("SD_SCHUL_CODE").asInt,
+                    schoolName = get("SCHUL_NM").asString,
+                    mealCode = get("MMEAL_SC_CODE").asInt,
+                    mealName = get("MMEAL_SC_NM").asString,
+                    date = get("MLSV_YMD").asString,
+                    numberStudents = get("MLSV_FGR").asInt,
+                    menu = get("DDISH_NM").asString.splitBrAndTrim(),
+                    originCountries = get("ORPLC_INFO").asString.splitBrAndTrim(),
+                    calorie = parseCalorie(get("CAL_INFO").asString),
+                    nutrients = get("NTR_INFO").asString.splitBrAndTrim(),
+                    fromDate = get("MLSV_FROM_YMD").asString,
+                    endDate = get("MLSV_TO_YMD").asString
+                )
+            }
+        }
+    }
+
+    fun parseCalorie(calorieString: String): Double {
+        if (!calorieString.contains("Kcal")) throw MealDeserializerException("Calorie string doesn't contain \"Kcal\".")
+        return calorieString.split(" ")[0].toDouble()
+    }
+
+}
+
+class MealDeserializerException(override val message: String) : Exception(message)
+
+fun JsonElement.getAsJsonObject(key: String): JsonElement = this.asJsonObject.get(key)
+
+fun String.splitBrAndTrim(): List<String> =
+    split("<br/>")
+        .map { it.trim() }
+        .filter { it.isNotEmpty() }
