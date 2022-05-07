369bc348b50df6eb6127b5e61f35ccd8b41c7d7d
Wooyoung Myung <mwy3055@gmail.com>
2022-05-07 14:43:26 +0900

    feature: 하나의 급식 메뉴를 나타내는 Menu 클래스 구현 (coverage: 95%)
    Menu는 요리의 이름과 알레르기를 일으킬 수 있는 요인으로 구성된다.

---
+++ app/src/test/java/com/practice/hanbitlunch/api/meal/MenuTest.kt
@@ -0,0 +1,15 @@
+package com.practice.hanbitlunch.api.meal
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class MenuTest {
+
+    @Test
+    fun testAllergicKorean() {
+        val allergic = listOf(1, 3, 8)
+        val expected = listOf("난류", "메밀", "게")
+        val actual = Menu(name = "test", allergic = allergic).allergicKorean
+        assertEquals(expected, actual)
+    }
+}
