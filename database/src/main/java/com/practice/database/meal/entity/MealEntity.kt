1814609f33c94bcdf0d872e2b9d5f44c51a7f06e
Wooyoung Myung <mwy3055@gmail.com>
2022-06-23 20:22:21 +0900

    feature: declare room entities

--- database/src/main/java/com/practice/database/meal/entity/MealEntity.kt
+++ database/src/main/java/com/practice/database/meal/entity/MealEntity.kt
@@ -1,5 +1,10 @@
 package com.practice.database.meal.entity
 
+import com.hsk.ktx.getDateString
+import com.hsk.ktx.jsonToList
+import com.hsk.ktx.toJson
+import com.practice.database.meal.room.MealEntityRoom
+
 data class MealEntity(
     val schoolCode: Int,
     val year: Int,
@@ -10,3 +15,30 @@ data class MealEntity(
     val calorie: Double,
     val nutrients: List<NutrientEntity>,
 )
+
+
+fun MealEntity.toRoomEntity() = MealEntityRoom(
+    date = getDateString(year, month, day),
+    menu = menus.toJson(),
+    origin = origins.toJson(),
+    calorie = calorie,
+    nutrient = nutrients.toJson(),
+    schoolCode = schoolCode,
+)
+
+fun MealEntityRoom.toMealEntity(): MealEntity {
+    return MealEntity(
+        schoolCode = schoolCode,
+        year = date.slice(0..3).toInt(),
+        month = date.slice(4..5).toInt(),
+        day = date.slice(6..7).toInt(),
+        menus = menu.jsonToList(),
+        origins = origin.jsonToList(),
+        calorie = calorie,
+        nutrients = nutrient.jsonToList(),
+    )
+}
+
+fun List<MealEntity>.toRoomEntities() = map { it.toRoomEntity() }
+
+fun List<MealEntityRoom>.toMealEntities() = map { it.toMealEntity() }
