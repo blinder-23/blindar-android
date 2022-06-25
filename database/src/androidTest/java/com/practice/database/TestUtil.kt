aeab28505bc1f1082fe4ea695ada844adf23d8fe
Wooyoung Myung <mwy3055@gmail.com>
2022-06-25 14:29:41 +0900

    test: add test utility function

--- database/src/androidTest/java/com/practice/database/TestUtil.kt
+++ database/src/androidTest/java/com/practice/database/TestUtil.kt
@@ -1,5 +1,9 @@
 package com.practice.database
 
+import com.practice.database.meal.entity.MealEntity
+import com.practice.database.meal.entity.MenuEntity
+import com.practice.database.meal.entity.NutrientEntity
+import com.practice.database.meal.entity.OriginEntity
 import com.practice.database.meal.room.MealEntityRoom
 
 object TestUtil {
@@ -14,5 +18,18 @@ object TestUtil {
                 schoolCode = 0
             )
         }
+    }
+
+    fun createMealEntity(count: Int): List<MealEntity> = (0 until count).map {
+        MealEntity(
+            schoolCode = 10,
+            year = 2022,
+            month = 5,
+            day = 1 + it,
+            menus = listOf(MenuEntity(name = "menu", allergies = emptyList())),
+            origins = listOf(OriginEntity(ingredient = "rice", origin = "korea")),
+            calorie = 10.0,
+            nutrients = listOf(NutrientEntity("protein", "g", 10.0))
+        )
     }
 }
