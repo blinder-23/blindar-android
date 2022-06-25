dd806831435c00a03799d1e15e9ddfac8b04d3f3
Wooyoung Myung <mwy3055@gmail.com>
2022-06-25 16:57:59 +0900

    feature: create Room DAO object for schedule

--- database/src/androidTest/java/com/practice/database/TestUtil.kt
+++ database/src/androidTest/java/com/practice/database/TestUtil.kt
@@ -5,6 +5,8 @@ import com.practice.database.meal.entity.MenuEntity
 import com.practice.database.meal.entity.NutrientEntity
 import com.practice.database.meal.entity.OriginEntity
 import com.practice.database.meal.room.MealEntityRoom
+import com.practice.database.schedule.entity.ScheduleEntity
+import com.practice.database.schedule.room.ScheduleEntityRoom
 
 object TestUtil {
     fun createMealEntityRoom(count: Int): List<MealEntityRoom> {
@@ -30,6 +32,26 @@ object TestUtil {
             origins = listOf(OriginEntity(ingredient = "rice", origin = "korea")),
             calorie = 10.0,
             nutrients = listOf(NutrientEntity("protein", "g", 10.0))
+        )
+    }
+
+    fun createScheduleEntityRoom(count: Int): List<ScheduleEntityRoom> = (0 until count).map {
+        ScheduleEntityRoom(
+            date = (20220601 + it).toString(),
+            schoolCode = 100,
+            eventName = "event $it",
+            eventContent = "content $it",
+        )
+    }
+
+    fun createScheduleEntity(count: Int): List<ScheduleEntity> = (0 until count).map {
+        ScheduleEntity(
+            schoolCode = 100,
+            year = 2022,
+            month = 6,
+            day = 1 + it,
+            eventName = "event $it",
+            eventContent = "content $it"
         )
     }
 }
---
+++ database/src/main/java/com/practice/database/schedule/room/ScheduleDao.kt
@@ -0,0 +1,23 @@
+package com.practice.database.schedule.room
+
+import androidx.room.*
+
+@Dao
+interface ScheduleDao {
+
+    @Query("SELECT * FROM schedule WHERE date LIKE :yearMonth || '%%'")
+    suspend fun getSchedule(yearMonth: String): List<ScheduleEntityRoom>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertSchedules(schedules: List<ScheduleEntityRoom>)
+
+    @Delete
+    suspend fun deleteSchedules(schedules: List<ScheduleEntityRoom>)
+
+    @Query("DELETE FROM schedule WHERE date LIKE :yearMonth || '%%'")
+    suspend fun deleteSchedules(yearMonth: String)
+
+    @Query("DELETE FROM schedule")
+    suspend fun clear()
+
+}
