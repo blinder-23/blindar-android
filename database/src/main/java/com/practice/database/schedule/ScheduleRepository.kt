f697f9c6262e5d86622cd9cbb48ffa7874650352
Wooyoung Myung <mwy3055@gmail.com>
2022-06-26 15:04:49 +0900

    feature: create schedule repository (implemented by local data source)

---
+++ database/src/main/java/com/practice/database/schedule/ScheduleRepository.kt
@@ -0,0 +1,35 @@
+package com.practice.database.schedule
+
+import com.practice.database.schedule.entity.ScheduleEntity
+
+class ScheduleRepository(private val dataSource: ScheduleLocalDataSource) {
+
+    suspend fun getSchedules(year: Int, month: Int): List<ScheduleEntity> {
+        return dataSource.getSchedules(year, month)
+    }
+
+    suspend fun insertSchedules(schedules: List<ScheduleEntity>) {
+        dataSource.insertSchedules(schedules)
+    }
+
+    suspend fun insertSchedules(vararg schedules: ScheduleEntity) {
+        insertSchedules(schedules.asList())
+    }
+
+    suspend fun deleteSchedules(schedules: List<ScheduleEntity>) {
+        dataSource.deleteSchedules(schedules)
+    }
+
+    suspend fun deleteSchedules(vararg schedules: ScheduleEntity) {
+        deleteSchedules(schedules.asList())
+    }
+
+    suspend fun deleteSchedules(year: Int, month: Int) {
+        dataSource.deleteSchedules(year, month)
+    }
+
+    suspend fun clear() {
+        dataSource.clear()
+    }
+
+}
