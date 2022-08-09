25c7fc25df87c8befdbec990bdaf486abe60c4c1
Wooyoung Myung <mwy3055@gmail.com>
2022-08-09 12:18:05 +0900

    feature: add hilt module which provides use cases

---
+++ domain/di/src/androidTest/java/com/example/di/UseCaseModuleTest.kt
@@ -0,0 +1,27 @@
+package com.example.di
+
+import com.example.domain.LoadMealScheduleDataUseCase
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Rule
+import org.junit.Test
+import javax.inject.Inject
+
+@HiltAndroidTest
+class UseCaseModuleTest {
+
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var useCase: LoadMealScheduleDataUseCase
+
+    @Test
+    fun injectUseCase() {
+        assertThat(::useCase.isInitialized).isFalse
+        hiltRule.inject()
+        assertThat(::useCase.isInitialized).isTrue
+    }
+
+}
