2813736d56e04d62197c9730b582a3623620c7b3
Wooyoung Myung <mwy3055@gmail.com>
2022-08-09 12:34:17 +0900

    feature: hilt module now provides singleton

--- database/di/src/main/java/com/practice/di/schedule/ScheduleRoomModule.kt
+++ database/di/src/main/java/com/practice/di/schedule/ScheduleRoomModule.kt
@@ -9,10 +9,13 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ScheduleRoomModule {
+
+    @Singleton
     @Provides
     fun provideScheduleDatabase(
         @ApplicationContext context: Context
