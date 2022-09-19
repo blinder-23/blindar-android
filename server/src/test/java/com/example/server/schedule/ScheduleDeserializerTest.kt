06c636d7299e304c1eea2190aba0c277f63f6f9a
Wooyoung Myung <mwy3055@gmail.com>
2022-09-19 14:54:32 +0900

    test: json 파서를 테스트하는 코드 작성
    Json 예시 파일을 파싱하는 코드를 작성하였다. 테스트는 이 정도면 충분한 듯하다.

---
+++ server/src/test/java/com/example/server/schedule/ScheduleDeserializerTest.kt
@@ -0,0 +1,57 @@
+package com.example.server.schedule
+
+import com.example.server.schedule.pojo.ScheduleDeserializer
+import com.example.server.schedule.pojo.ScheduleModel
+import com.example.server.schedule.pojo.ScheduleResponse
+import com.google.gson.JsonParser
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.util.*
+
+class ScheduleDeserializerTest {
+
+    private val deserializer = ScheduleDeserializer()
+    private val json = """
+        {
+            "response": [
+                {
+                    "id": 1149108,
+                    "date": 1662044400,
+                    "schedule": "봉사활동 사전 교육"
+                },
+                {
+                    "id": 1149109,
+                    "date": 1662303600,
+                    "schedule": "초등부 '독서기록 꿈자람' 시작"
+                }
+            ]
+        }
+    """.trimIndent()
+    private val answer = ScheduleResponse(
+        listOf(
+            ScheduleModel(
+                id = 1149108,
+                date = 1662044400L,
+                title = "봉사활동 사전 교육"
+            ),
+            ScheduleModel(
+                id = 1149109,
+                date = 1662303600L,
+                title = "초등부 '독서기록 꿈자람' 시작"
+            )
+        )
+    )
+
+    @Test
+    fun `test by sample json`() {
+        val jsonObj = JsonParser.parseString(json).asJsonObject
+        val actual = deserializer.deserializeSchedules(jsonObj.get("response").asJsonArray)
+        assertThat(actual).isEqualTo(answer)
+        // Fri Sep 02 00:00:00 KST 2022
+        // it.date is second, but Date receives millisecond
+        actual.schedules.forEach {
+            println(Date(it.date * 1000))
+        }
+    }
+
+}
