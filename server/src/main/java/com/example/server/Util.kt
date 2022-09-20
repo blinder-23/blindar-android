4ddb3448a60f11b090944d738de0135eb308bee1
Wooyoung Myung <mwy3055@gmail.com>
2022-09-20 14:09:38 +0900

    test: 서버에서 학사일정을 가져오는 Repository 테스트 코드 작성

---
+++ server/src/main/java/com/example/server/Util.kt
@@ -0,0 +1,9 @@
+package com.example.server
+
+import java.time.LocalDate
+
+/**
+ * Convert epoch second to epoch day.
+ * Can be used with [LocalDate.ofEpochDay].
+ */
+fun Long.toEpochDay() = this / 86400
