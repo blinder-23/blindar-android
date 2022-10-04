bf54a3ba58bf8b44e41ac5a989bb7ff6da84a451
Wooyoung Myung <mwy3055@gmail.com>
2022-10-04 14:20:53 +0900

    feature: Remote meal 관련 객체를 주입하는 Hilt module 작성

---
+++ server/di/src/main/java/com/example/di/meal/MealApiModule.kt
@@ -0,0 +1,18 @@
+package com.example.di.meal
+
+import com.example.server.meal.api.MealApi
+import com.example.server.meal.api.mealApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object MealApiModule {
+
+    @Singleton
+    @Provides
+    fun provideMealApi(): MealApi = mealApi
+}
