871e82944b1e092528a02acf0df4cdb92f4623fb
Wooyoung Myung <mwy3055@gmail.com>
2022-10-04 14:11:04 +0900

    refactor: DataSource를 테스트하는 코드의 파일명이 repository로 되어 있던 문제를 수정

--- server/src/test/java/com/example/server/schedule/RemoteScheduleDataSourceImplTest.kt
+++ server/src/test/java/com/example/server/schedule/RemoteScheduleDataSourceImplTest.kt
@@ -1,24 +1,27 @@
 package com.example.server.schedule
 
 import com.example.server.schedule.api.scheduleApi
+import com.example.server.toEpochDate
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.time.LocalDate
 import java.time.Month
 
 class RemoteScheduleRepositoryImplTest {
 
-    private val repository: RemoteScheduleDataSource = RemoteScheduleDataSourceImpl(scheduleApi)
+    private val dataSource: RemoteScheduleDataSource = RemoteScheduleDataSourceImpl(scheduleApi)
 
     @Test
     fun `test schedules`(): Unit = runBlocking {
         val year = 2022
         val month = 9
-        val result = repository.getSchedules(year, month)
+        val result = dataSource.getSchedules(year, month)
 
         assertThat(result.schedules).isNotEmpty
-            .allSatisfy { LocalDate.ofEpochDay(9).month == Month.SEPTEMBER }
+            .allSatisfy {
+                val scheduleMonth = it.date.toEpochDate(9).month
+                assertThat(scheduleMonth).isEqualTo(Month.SEPTEMBER)
+            }
     }
 
 }
