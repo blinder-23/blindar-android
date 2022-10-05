63b2ca270ddc9c257c5042dfaf7c7f617de7c2d0
Wooyoung Myung <mwy3055@gmail.com>
2022-10-05 11:37:31 +0900

    log: 테스트용 로그 찍는 코드 더 추가

--- database/src/main/java/com/practice/database/meal/LocalMealDataSource.kt
+++ database/src/main/java/com/practice/database/meal/LocalMealDataSource.kt
@@ -1,5 +1,6 @@
 package com.practice.database.meal
 
+import android.util.Log
 import com.hsk.ktx.getDateString
 import com.practice.database.meal.entity.MealEntity
 import com.practice.database.meal.room.MealDao
@@ -11,10 +12,14 @@ import kotlinx.coroutines.flow.map
 class LocalMealDataSource(private val mealDao: MealDao) : MealDataSource {
 
     override suspend fun getMeals(year: Int, month: Int): Flow<List<MealEntity>> {
-        return mealDao.getMeals(getDateString(year, month)).map { it.toMealEntities() }
+        return mealDao.getMeals(getDateString(year, month)).map {
+            Log.d("LocalMealDataSource", "meal $year $month: ${it.size}")
+            it.toMealEntities()
+        }
     }
 
     override suspend fun insertMeals(meals: List<MealEntity>) {
+        Log.d("LocalMealDataSource", "Inserted: ${meals[0].year} ${meals[0].month} ${meals.size}")
         mealDao.insertMeals(meals.toRoomEntities())
     }
 
