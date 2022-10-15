0ab0ef914583b85b0cf5bfd9e655413acf1decc3
Wooyoung Myung <mwy3055@gmail.com>
2022-10-15 16:29:23 +0900

    feature: 학사일정 내용 필드 추가

--- server/src/main/java/com/example/server/schedule/pojo/ScheduleModel.kt
+++ server/src/main/java/com/example/server/schedule/pojo/ScheduleModel.kt
@@ -1,7 +1,10 @@
 package com.example.server.schedule.pojo
 
+import com.google.gson.annotations.SerializedName
+
 data class ScheduleModel(
     val id: Int,
     val date: Long,
-    val title: String,
+    @SerializedName("schedule") val title: String,
+    val contents: String,
 )
