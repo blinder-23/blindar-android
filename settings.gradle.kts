307cd9701bfe3d73857f49aaf178562cdc358975
Wooyoung Myung <mwy3055@gmail.com>
2022-12-26 14:31:17 +0900

    gradle: kts 전환 및 version catalog 공용화

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven { url = java.net.URI.create("https://jitpack.io") }
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("../libs.versions.toml"))
+        }
+    }
+}
+rootProject.name = "Hanbit lunch"
+include(":app")
+include(":database")
+include(":preferences")
+include(":domain")
+include(":benchmark")
+include(":server")
+include(":work")
