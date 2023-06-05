72dac07c1358239b86459190f76d6d1a96ce0a55
Wooyoung Myung <mwy3055@gmail.com>
2023-06-05 22:29:28 +0900

    feature: 회원가입 nested navigation 추가

--- app/src/main/java/com/practice/hanbitlunch/screen/BlindarNavHost.kt
+++ app/src/main/java/com/practice/hanbitlunch/screen/BlindarNavHost.kt
@@ -12,7 +12,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.IntOffset
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
+import androidx.navigation.navigation
 import com.google.accompanist.navigation.animation.AnimatedNavHost
 import com.google.accompanist.navigation.animation.composable
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
@@ -20,6 +22,7 @@ import com.practice.login.LoginScreen
 import com.practice.main.MainScreen
 import com.practice.onboarding.onboarding.OnboardingScreen
 import com.practice.onboarding.splash.SplashScreen
+import com.practice.register.phonenumber.VerifyPhoneNumber
 import com.practice.register.registerform.RegisterFormScreen
 import com.practice.register.selectschool.SelectSchoolScreen
 import kotlinx.coroutines.delay
@@ -64,37 +67,80 @@ fun BlindarNavHost(
             }
         },
     ) {
-        composable(SPLASH) {
-            SplashScreen(
-                login = {
-                    // auto login code
-                    delay(1000L)
-                    false
-                },
-                onLoginSuccess = {
-                    navController.navigate(MAIN) {
-                        popUpTo(SPLASH) { inclusive = true }
+        blindarMainNavGraph(navController, windowSizeClass)
+    }
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+fun NavGraphBuilder.blindarMainNavGraph(
+    navController: NavHostController,
+    windowSizeClass: WindowSizeClass
+) {
+    composable(SPLASH) {
+        SplashScreen(
+            login = {
+                // auto login code
+                delay(1000L)
+                false
+            },
+            onLoginSuccess = {
+                navController.navigate(MAIN) {
+                    popUpTo(SPLASH) { inclusive = true }
+                }
+            },
+            onLoginFail = {
+                navController.navigate(ONBOARDING) {
+                    popUpTo(SPLASH) { inclusive = true }
+                }
+            },
+            modifier = Modifier.fillMaxSize(),
+        )
+    }
+    composable(ONBOARDING) {
+        OnboardingScreen(
+            onRegister = { navController.navigate(REGISTER) },
+            onGoogleLogin = { /*TODO*/ },
+            onLogin = { navController.navigate(LOGIN) },
+            modifier = Modifier.fillMaxSize(),
+        )
+    }
+    registerGraph(navController)
+    composable(LOGIN) {
+        LoginScreen(
+            onLoginSuccess = {
+                navController.navigate(MAIN) {
+                    popUpTo(ONBOARDING) { inclusive = true }
+                }
+            },
+            modifier = Modifier.fillMaxSize(),
+        )
+    }
+    composable(MAIN) {
+        MainScreen(
+            windowSize = windowSizeClass,
+            viewModel = hiltViewModel(),
+            modifier = Modifier.fillMaxSize(),
+        )
+    }
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+fun NavGraphBuilder.registerGraph(navController: NavHostController) {
+    navigation(startDestination = VERIFY_PHONE, route = REGISTER) {
+        composable(VERIFY_PHONE) {
+            VerifyPhoneNumber(
+                onNextButtonClick = {
+                    navController.navigate(REGISTER_FORM) {
+
                     }
                 },
-                onLoginFail = {
-                    navController.navigate(ONBOARDING) {
-                        popUpTo(SPLASH) { inclusive = true }
-                    }
-                },
-                modifier = Modifier.fillMaxSize(),
-            )
-        }
-        composable(ONBOARDING) {
-            OnboardingScreen(
-                onRegister = { navController.navigate(REGISTER_FORM) },
-                onGoogleLogin = { /*TODO*/ },
-                onLogin = { navController.navigate(LOGIN) },
                 modifier = Modifier.fillMaxSize(),
             )
         }
         composable(REGISTER_FORM) {
             // RegisterForm과 SelectSchool은 같은 ViewModel을 공유
             RegisterFormScreen(
+                onLaunchRegisterUI = {},
                 onNextButtonClick = {
                     navController.navigate(SELECT_SCHOOL)
                 },
@@ -111,28 +157,13 @@ fun BlindarNavHost(
                 modifier = Modifier.fillMaxSize(),
             )
         }
-        composable(LOGIN) {
-            LoginScreen(
-                onLoginSuccess = {
-                    navController.navigate(MAIN) {
-                        popUpTo(ONBOARDING) { inclusive = true }
-                    }
-                },
-                modifier = Modifier.fillMaxSize(),
-            )
-        }
-        composable(MAIN) {
-            MainScreen(
-                windowSize = windowSizeClass,
-                viewModel = mainScreenViewModel,
-                modifier = Modifier.fillMaxSize(),
-            )
-        }
     }
 }
 
 private const val SPLASH = "splash"
 private const val ONBOARDING = "onboarding"
+private const val REGISTER = "register"
+private const val VERIFY_PHONE = "verify_phone"
 private const val REGISTER_FORM = "register_form"
 private const val SELECT_SCHOOL = "select_school"
 private const val LOGIN = "login"
@@ -144,11 +175,12 @@ private val NavBackStackEntry.route: String?
 private fun priority(state: NavBackStackEntry) = when (state.route) {
     SPLASH -> 0
     ONBOARDING -> 1
-    REGISTER_FORM -> 2
-    SELECT_SCHOOL -> 3
-    LOGIN -> 4
-    MAIN -> 5
-    else -> 6
+    VERIFY_PHONE -> 2
+    REGISTER_FORM -> 3
+    SELECT_SCHOOL -> 4
+    LOGIN -> 5
+    MAIN -> 6
+    else -> 7
 }
 
 @OptIn(ExperimentalAnimationApi::class)
