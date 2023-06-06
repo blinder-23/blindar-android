1e2a407262f1a7867f33df1fca67ad020a80d832
Wooyoung Myung <mwy3055@gmail.com>
2023-06-06 16:52:22 +0900

    feature: 휴대폰 인증 창에서 사용할 ui state 정의

---
+++ feature/register/src/main/java/com/practice/register/RegisterUiState.kt
@@ -0,0 +1,22 @@
+package com.practice.register
+
+import com.practice.register.phonenumber.PhoneNumberValidator
+
+data class RegisterUiState(
+    val phoneNumber: String,
+    val authCode: String,
+    val isAuthCodeFieldEnabled: Boolean,
+    val isVerifyCodeButtonEnabled: Boolean,
+) {
+    val isPhoneNumberValid: Boolean
+        get() = if (phoneNumber.isEmpty()) true else PhoneNumberValidator.validate(phoneNumber)
+
+    companion object {
+        val Empty = RegisterUiState(
+            phoneNumber = "",
+            authCode = "",
+            isAuthCodeFieldEnabled = false,
+            isVerifyCodeButtonEnabled = false,
+        )
+    }
+}
