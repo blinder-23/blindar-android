1a9a9ecd3bd8030586b5b341f6c933267cf3255a
Wooyoung Myung <mwy3055@gmail.com>
2023-06-08 23:37:43 +0900

    feature: Remote School DataSource, Repository 정의

---
+++ core/api/src/main/java/com/practice/api/school/FakeRemoteSchoolDataSource.kt
@@ -0,0 +1,17 @@
+package com.practice.api.school
+
+import com.practice.api.school.pojo.SchoolModel
+import kotlinx.coroutines.delay
+import javax.inject.Inject
+
+class FakeRemoteSchoolDataSource @Inject constructor() : RemoteSchoolDataSource {
+    override suspend fun getSupportedSchools(): List<SchoolModel> {
+        delay(500L)
+        return (1..15).map {
+            SchoolModel(
+                name = "학교 $it",
+                schoolId = it.toString(),
+            )
+        }
+    }
+}
