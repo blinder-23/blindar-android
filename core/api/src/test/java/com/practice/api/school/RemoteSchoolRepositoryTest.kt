22a2511383ae71f3ee601cdd2f7094169fe25cac
Wooyoung Myung <mwy3055@gmail.com>
2023-06-08 23:38:19 +0900

    test: Remote School 테스트 코드 작성

---
+++ core/api/src/test/java/com/practice/api/school/RemoteSchoolRepositoryTest.kt
@@ -0,0 +1,36 @@
+package com.practice.api.school
+
+import kotlinx.coroutines.test.runTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class RemoteSchoolRepositoryTest {
+    private val fakeRemoteSchoolDataSource = FakeRemoteSchoolDataSource()
+    private val remoteSchoolRepository = RemoteSchoolRepository(fakeRemoteSchoolDataSource)
+
+    @Test
+    fun getAll() = runTest {
+        val expected = fakeRemoteSchoolDataSource.getSupportedSchools()
+        val actual = remoteSchoolRepository.getSupportedSchools()
+        assertThat(expected)
+            .containsExactlyInAnyOrderElementsOf(actual)
+    }
+
+    @Test
+    fun emptyQuery() = runTest {
+        val expected = fakeRemoteSchoolDataSource.getSupportedSchools()
+        val actual = remoteSchoolRepository.searchSupportedSchools("")
+        assertThat(expected)
+            .containsExactlyInAnyOrderElementsOf(actual)
+    }
+
+    @Test
+    fun query() = runTest {
+        val query = "2"
+        val expected =
+            fakeRemoteSchoolDataSource.getSupportedSchools().filter { it.name.contains(query) }
+        val actual = remoteSchoolRepository.searchSupportedSchools(query)
+        assertThat(expected)
+            .containsExactlyInAnyOrderElementsOf(actual)
+    }
+}
