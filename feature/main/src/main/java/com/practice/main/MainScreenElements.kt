a9595a98f94a5655a875abd6a9da6efcd4b33828
Wooyoung Myung <mwy3055@gmail.com>
2023-06-14 17:27:27 +0900

    feature: 달력 하단의 내용을 스크롤할 수 있도록 수정

--- feature/main/src/main/java/com/practice/main/MainScreenElements.kt
+++ feature/main/src/main/java/com/practice/main/MainScreenElements.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -196,15 +197,20 @@ internal fun MainScreenContents(
     mealColumns: Int,
     modifier: Modifier = Modifier
 ) {
-    Column(
+    LazyColumn(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(10.dp),
+        contentPadding = PaddingValues(bottom = 10.dp),
     ) {
         if (!mealUiState.isEmpty) {
-            MealContent(mealUiState = mealUiState, columns = mealColumns)
+            item {
+                MealContent(mealUiState = mealUiState, columns = mealColumns)
+            }
         }
         if (!scheduleUiState.isEmpty) {
-            ScheduleContent(scheduleUiState)
+            item {
+                ScheduleContent(scheduleUiState)
+            }
         }
     }
 }
