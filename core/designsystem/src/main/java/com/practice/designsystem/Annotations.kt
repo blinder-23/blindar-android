b40f9358ae71c53692782e7a622135c09b21565f
Wooyoung Myung <mwy3055@gmail.com>
2023-07-19 15:22:59 +0900

    fix: 어노테이션 고침

--- core/designsystem/src/main/java/com/practice/designsystem/Annotations.kt
+++ core/designsystem/src/main/java/com/practice/designsystem/Annotations.kt
@@ -6,8 +6,8 @@ import androidx.compose.ui.tooling.preview.Preview
 @Preview(name = "Light", showBackground = true)
 annotation class LightPreview
 
-annotation class DarkPreview
 @Preview(name = "Dark", showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+annotation class DarkPreview
 
 @LightPreview
 @DarkPreview
