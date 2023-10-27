6b7c2633e3ec599bb78c553fb51c51ea13e0c117
Wooyoung Myung <mwy3055@gmail.com>
2023-10-27 10:18:25 +0900

    resource: M3 theme 전체 색깔 적용

--- core/designsystem/src/main/java/com/practice/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/practice/designsystem/theme/Color.kt
@@ -1,36 +1,66 @@
 package com.practice.designsystem.theme
 
-import androidx.compose.material3.ColorScheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import com.practice.designsystem.R
 
-private val BlindarBlue: Color
-    @Composable get() = colorResource(R.color.blindar_blue)
-private val BlindarBlueVariant: Color
-    @Composable get() = colorResource(R.color.blindar_blue_variant)
-private val BlindarYellow: Color
-    @Composable get() = colorResource(R.color.blindar_yellow)
+val md_theme_light_primary = Color(0xFF0062A2)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFFD1E4FF)
+val md_theme_light_onPrimaryContainer = Color(0xFF001D35)
+val md_theme_light_secondary = Color(0xFF245FA6)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFFD5E3FF)
+val md_theme_light_onSecondaryContainer = Color(0xFF001B3C)
+val md_theme_light_tertiary = Color(0xFF6E5D00)
+val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+val md_theme_light_tertiaryContainer = Color(0xFFFFE260)
+val md_theme_light_onTertiaryContainer = Color(0xFF221B00)
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_errorContainer = Color(0xFFFFDAD6)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
+val md_theme_light_outline = Color(0xFF73777F)
+val md_theme_light_background = Color(0xFFFDFCFF)
+val md_theme_light_onBackground = Color(0xFF1A1C1E)
+val md_theme_light_surface = Color(0xFFF9F9FC)
+val md_theme_light_onSurface = Color(0xFF1A1C1E)
+val md_theme_light_surfaceVariant = Color(0xFFDFE2EB)
+val md_theme_light_onSurfaceVariant = Color(0xFF42474E)
+val md_theme_light_inverseSurface = Color(0xFF2F3033)
+val md_theme_light_inverseOnSurface = Color(0xFFF1F0F4)
+val md_theme_light_inversePrimary = Color(0xFF9DCAFF)
+val md_theme_light_surfaceTint = Color(0xFF0062A2)
+val md_theme_light_outlineVariant = Color(0xFFC3C7CF)
+val md_theme_light_scrim = Color(0xFF000000)
 
-val LightColorPalette: ColorScheme
-    @Composable get() = lightColorScheme(
-        primary = BlindarBlue,
-        secondary = BlindarBlueVariant,
-        tertiary = BlindarYellow,
-        onPrimary = Color.White,
-        onSecondary = Color.White,
-        onTertiary = Color.Black,
-    )
+val md_theme_dark_primary = Color(0xFF9DCAFF)
+val md_theme_dark_onPrimary = Color(0xFF003257)
+val md_theme_dark_primaryContainer = Color(0xFF00497B)
+val md_theme_dark_onPrimaryContainer = Color(0xFFD1E4FF)
+val md_theme_dark_secondary = Color(0xFFA7C8FF)
+val md_theme_dark_onSecondary = Color(0xFF003061)
+val md_theme_dark_secondaryContainer = Color(0xFF004689)
+val md_theme_dark_onSecondaryContainer = Color(0xFFD5E3FF)
+val md_theme_dark_tertiary = Color(0xFFE3C539)
+val md_theme_dark_onTertiary = Color(0xFF3A3000)
+val md_theme_dark_tertiaryContainer = Color(0xFF534600)
+val md_theme_dark_onTertiaryContainer = Color(0xFFFFE260)
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_errorContainer = Color(0xFF93000A)
+val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+val md_theme_dark_outline = Color(0xFF8C9199)
+val md_theme_dark_background = Color(0xFF1A1C1E)
+val md_theme_dark_onBackground = Color(0xFFE2E2E6)
+val md_theme_dark_surface = Color(0xFF121316)
+val md_theme_dark_onSurface = Color(0xFFC6C6CA)
+val md_theme_dark_surfaceVariant = Color(0xFF42474E)
+val md_theme_dark_onSurfaceVariant = Color(0xFFC3C7CF)
+val md_theme_dark_inverseSurface = Color(0xFFE2E2E6)
+val md_theme_dark_inverseOnSurface = Color(0xFF1A1C1E)
+val md_theme_dark_inversePrimary = Color(0xFF0062A2)
+val md_theme_dark_surfaceTint = Color(0xFF9DCAFF)
+val md_theme_dark_outlineVariant = Color(0xFF42474E)
+val md_theme_dark_scrim = Color(0xFF000000)
 
-val DarkColorPalette: ColorScheme
-    @Composable get() = darkColorScheme(
-        primary = BlindarBlue,
-        secondary = BlindarBlueVariant,
-        tertiary = BlindarYellow,
-        onPrimary = Color.White,
-        onSecondary = Color.White,
-        onTertiary = Color.Black,
-    )
+
+val seed = Color(0xFF0B4F82)
