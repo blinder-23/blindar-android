ab853e5909ffd13a133644f948bd9ca2adb5f71c
Wooyoung Myung <mwy3055@gmail.com>
2023-11-02 21:25:34 +0900

    feature: 메모 엔티티와 메모 도메인 객체 간 변환 코드 추가

---
+++ data/memo/src/main/java/com/practice/memo/room/DomainConverter.kt
@@ -0,0 +1,23 @@
+package com.practice.memo.room
+
+import com.practice.domain.Memo
+
+fun MemoEntity.toMemo() = Memo(
+    id = id,
+    userId = userId,
+    year = date.slice(0..3).toInt(),
+    month = date.slice(4..5).toInt(),
+    day = date.slice(6..7).toInt(),
+    content = content,
+)
+
+fun Memo.toEntity(): MemoEntity {
+    val monthPadStartZero = month.toString().padStart(2, '0')
+    val dayPadStartZero = day.toString().padStart(2, '0')
+    return MemoEntity(
+        id = id,
+        userId = userId,
+        date = "$year$monthPadStartZero$dayPadStartZero",
+        content = content,
+    )
+}
