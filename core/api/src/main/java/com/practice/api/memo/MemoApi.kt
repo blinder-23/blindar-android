692b7789e6bbd8295a3bc72580b9a02828c362b2
Wooyoung Myung <mwy3055@gmail.com>
2023-11-05 22:10:19 +0900

    feature: 메모 Retrofit API 인터페이스 구현

---
+++ core/api/src/main/java/com/practice/api/memo/MemoApi.kt
@@ -0,0 +1,35 @@
+package com.practice.api.memo
+
+import com.practice.api.ServerRetrofit
+import com.practice.api.memo.pojo.DeleteMemoResponse
+import com.practice.api.memo.pojo.GetMemoResponse
+import com.practice.api.memo.pojo.RemoteMemoEntity
+import com.practice.api.memo.pojo.UpdateMemoResponse
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface MemoApi {
+    @POST("memo")
+    suspend fun updateMemo(
+        @Body body: RemoteMemoEntity,
+    ): UpdateMemoResponse
+
+    @DELETE("memo/{memo_id}")
+    suspend fun deleteMemo(
+        @Path("memo_id", encoded = true) memoId: String
+    ): DeleteMemoResponse
+
+    @GET("memo")
+    suspend fun getAllMemoOfUser(
+        @Query("user_id") userId: String,
+    ): GetMemoResponse
+}
+
+val memoApi: MemoApi
+    get() = ServerRetrofit.getRetrofit(GsonConverterFactory.create())
+        .create(MemoApi::class.java)
