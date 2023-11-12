b06a1dc8cdb145ab9f56e2fd00ec3a7c1c50eab9
Wooyoung Myung <mwy3055@gmail.com>
2023-11-12 14:55:30 +0900

    feature: 연, 월, 일을 YYYYMMDD 형식 문자열로 변환하는 함수 추가

--- core/util/src/main/java/com/practice/util/date/DateUtil.kt
+++ core/util/src/main/java/com/practice/util/date/DateUtil.kt
@@ -4,4 +4,11 @@ import com.hsk.ktx.date.Date
 
 object DateUtil {
     fun today() = Date.now()
+
+    fun toDateString(year: Int, month: Int, dayOfMonth: Int): String {
+        val formattedYear = year.toString().padStart(4, '0')
+        val formattedMonth = month.toString().padStart(2, '0')
+        val formattedDayOfMonth = dayOfMonth.toString().padStart(2, '0')
+        return "$formattedYear$formattedMonth$formattedDayOfMonth"
+    }
 }
