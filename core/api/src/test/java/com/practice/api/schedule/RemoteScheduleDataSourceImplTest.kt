46d2ff5365098dd32046e62130b0e6eacbc273ce
Wooyoung Myung <mwy3055@gmail.com>
2023-11-26 20:09:48 +0900

    test: 서버 API가 복구되었으므로 API 테스트도 주석 해제

--- core/api/src/test/java/com/practice/api/schedule/RemoteScheduleDataSourceImplTest.kt
+++ core/api/src/test/java/com/practice/api/schedule/RemoteScheduleDataSourceImplTest.kt
@@ -1,23 +1,26 @@
 package com.practice.api.schedule
 
 import com.practice.api.schedule.api.scheduleApi
+import com.practice.api.toEpochDate
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 
 class RemoteScheduleRepositoryImplTest {
 
     private val dataSource: RemoteScheduleDataSource = RemoteScheduleDataSourceImpl(scheduleApi)
 
-    // TODO: 학사일정 api 고쳐지면 다시 켜기
-//    @Test
-//    fun `test schedules`(): Unit = runBlocking {
-//        val year = 2022
-//        val month = 9
-//        val result = dataSource.getSchedules(7010578, year, month)
-//
-//        assertThat(result.schedules).isNotEmpty
-//            .allSatisfy {
-//                val scheduleMonth = it.date.toEpochDate(9).month
-//                assertThat(scheduleMonth).isEqualTo(9)
-//            }
-//    }
+    @Test
+    fun `test schedules`(): Unit = runBlocking {
+        val year = 2022
+        val month = 9
+        val result = dataSource.getSchedules(7010578, year, month)
+
+        assertThat(result.schedules).isNotEmpty
+            .allSatisfy {
+                val scheduleMonth = it.date.toEpochDate(9).month
+                assertThat(scheduleMonth).isEqualTo(9)
+            }
+    }
 
 }
