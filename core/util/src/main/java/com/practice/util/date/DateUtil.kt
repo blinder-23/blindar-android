f9fdf70edbcf8ab9c9e494ff50718ab936d56493
Wooyoung Myung <mwy3055@gmail.com>
2023-12-14 17:15:58 +0900

    feature: 주어진 시간이 돌아오는 다음 시점와의 차이를 반환하는 유틸 코드 추가

--- core/util/src/main/java/com/practice/util/date/DateUtil.kt
+++ core/util/src/main/java/com/practice/util/date/DateUtil.kt
@@ -1,6 +1,8 @@
 package com.practice.util.date
 
 import com.hsk.ktx.date.Date
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 
 object DateUtil {
     fun today() = Date.now()
@@ -10,5 +12,18 @@ object DateUtil {
         val formattedMonth = month.toString().padStart(2, '0')
         val formattedDayOfMonth = dayOfMonth.toString().padStart(2, '0')
         return "$formattedYear$formattedMonth$formattedDayOfMonth"
+    }
+
+    fun getNextTimeOffsetInMillis(hour: Int, minute: Int, second: Int): Long {
+        val now = LocalDateTime.now()
+        val zone = ZoneOffset.systemDefault()
+        val time = LocalDateTime.now().withHour(hour).withMinute(minute).withSecond(second)
+        val nextTime = if (time.atZone(zone).toEpochSecond() <= now.atZone(zone).toEpochSecond()) {
+            time.plusDays(1)
+        } else {
+            time
+        }
+
+        return nextTime.atZone(zone).toEpochSecond() - now.atZone(zone).toEpochSecond()
     }
 }
