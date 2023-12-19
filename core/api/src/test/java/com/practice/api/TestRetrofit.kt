0ba00aa2a792ec5dfb9cb92c8fd6c5584ec5fcd6
Wooyoung Myung <mwy3055@gmail.com>
2023-12-19 11:26:03 +0900

    test: 테스트 코드 전용 Retrofit을 만드는 객체 추가

---
+++ core/api/src/test/java/com/practice/api/TestRetrofit.kt
@@ -0,0 +1,20 @@
+package com.practice.api
+
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object TestRetrofit {
+    // TODO: 테스트 코드에 있는 retrofit을 전부 이걸로 바꾸기
+    fun <T> getRetrofit(
+        clazz: Class<T>,
+        factory: Converter.Factory = GsonConverterFactory.create(),
+    ): T = getTestRetrofit(factory).create(clazz)
+
+    private fun getTestRetrofit(factory: Converter.Factory) = Retrofit.Builder()
+        .baseUrl(TEST_SERVER_URL)
+        .addConverterFactory(factory)
+        .build()
+
+    private const val TEST_SERVER_URL = "http://192.168.0.10"
+}
