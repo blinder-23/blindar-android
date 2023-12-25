03dcddbc9412c91f647b6e7328038612d2492392
Wooyoung Myung <mwy3055@gmail.com>
2023-12-25 15:35:01 +0900

    remove: SplashViewModel에서 사용하지 않는 코드 제거

--- feature/onboarding/src/main/java/com/practice/onboarding/splash/SplashViewModel.kt
+++ feature/onboarding/src/main/java/com/practice/onboarding/splash/SplashViewModel.kt
@@ -1,31 +1,17 @@
 package com.practice.onboarding.splash
 
 import android.content.Context
-import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.practice.preferences.PreferencesRepository
 import com.practice.user.RegisterManager
 import com.practice.user.UserRegisterState
 import com.practice.work.BlindarWorkManager
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
-    private val preferencesRepository: PreferencesRepository,
     private val registerManager: RegisterManager,
 ) : ViewModel() {
-    suspend fun enqueueOneTimeWorkIfFirstExecution(context: Context) {
-        val firstPreferences = preferencesRepository.userPreferencesFlow.first()
-        if (firstPreferences.isFirstExecution) {
-            BlindarWorkManager.setOneTimeFetchDataWork(context = context)
-            preferencesRepository.updateIsFirstExecution(false)
-            Log.d(TAG, "first work enqueued.")
-        }
-        BlindarWorkManager.setPeriodicFetchDataWork(context)
-        Log.d(TAG, "Let's login!")
-    }
 
     suspend fun getUserRegisterState(): UserRegisterState {
         return registerManager.getUserRegisterState()
