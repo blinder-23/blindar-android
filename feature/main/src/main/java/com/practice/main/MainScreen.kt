1f8588db524f6821151eb2f622c1634c2e0364a2
Wooyoung Myung <mwy3055@gmail.com>
2024-01-07 20:01:16 +0900

    delete: 사용하지 않는 구버전 디자인 composable 삭제

--- feature/main/src/main/java/com/practice/main/MainScreen.kt
+++ feature/main/src/main/java/com/practice/main/MainScreen.kt
@@ -1,15 +1,7 @@
 package com.practice.main
 
-import androidx.compose.animation.core.CubicBezierEasing
-import androidx.compose.animation.core.InfiniteRepeatableSpec
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
@@ -17,21 +9,12 @@ import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.practice.designsystem.calendar.core.rememberCalendarState
-import com.practice.designsystem.theme.BlindarTheme
 import com.practice.main.popup.MemoPopup
 import com.practice.main.popup.popupPadding
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 @Composable
 fun MainScreen(
@@ -115,59 +98,5 @@ fun MainScreen(
                 modifier = Modifier.padding(popupPadding),
             )
         }
-    }
-}
-
-@Composable
-fun FloatingRefreshButton(
-    isLoading: Boolean,
-    onRefresh: () -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    val refreshIconAlpha by animateFloatAsState(targetValue = if (isLoading) 0.5f else 1f)
-    val infiniteTransition = rememberInfiniteTransition()
-    val angle by infiniteTransition.animateFloat(
-        initialValue = 0f,
-        targetValue = if (isLoading) 180f else 0f,
-        animationSpec = InfiniteRepeatableSpec(
-            animation = tween(
-                durationMillis = 750,
-                easing = CubicBezierEasing(0.3f, 0f, 0.7f, 1f),
-            ),
-        )
-    )
-
-    val interactionSource = remember { MutableInteractionSource() }
-    FloatingActionButton(
-        interactionSource = interactionSource,
-        onClick = {
-            if (!isLoading) onRefresh()
-        },
-        modifier = modifier
-            .rotate(angle),
-        containerColor = MaterialTheme.colorScheme.primary,
-    ) {
-        RefreshIcon(
-            iconAlpha = { refreshIconAlpha },
-        )
-    }
-}
-
-@Preview
-@Composable
-private fun FloatingRefreshButtonPreview() {
-    var isLoading by remember { mutableStateOf(false) }
-    val coroutineScope = rememberCoroutineScope()
-    BlindarTheme {
-        FloatingRefreshButton(
-            isLoading = isLoading,
-            onRefresh = {
-                isLoading = !isLoading
-                coroutineScope.launch {
-                    delay(2500L)
-                    isLoading = !isLoading
-                }
-            },
-        )
     }
 }
