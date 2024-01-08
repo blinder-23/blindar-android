030e5858511f2c2323ea4f99c088ca39bd724c81
Wooyoung Myung <mwy3055@gmail.com>
2024-01-08 17:38:46 +0900

    refactor: MainScreen 프리뷰용 MainUiState를 분리

--- feature/main/src/main/java/com/practice/main/calendar/HorizontalCalendarMainScreen.kt
+++ feature/main/src/main/java/com/practice/main/calendar/HorizontalCalendarMainScreen.kt
@@ -21,20 +21,11 @@ import com.practice.designsystem.calendar.core.YearMonth
 import com.practice.designsystem.calendar.core.rememberCalendarState
 import com.practice.designsystem.calendar.largeCalendarDateShape
 import com.practice.designsystem.theme.BlindarTheme
-import com.practice.domain.School
 import com.practice.main.MainScreenContents
 import com.practice.main.MainScreenTopBar
 import com.practice.main.R
-import com.practice.main.previewMemos
-import com.practice.main.previewMenus
-import com.practice.main.previewNutrients
-import com.practice.main.previewSchedules
-import com.practice.main.state.DailyData
-import com.practice.main.state.MainUiMode
+import com.practice.main.previewMainUiState
 import com.practice.main.state.MainUiState
-import com.practice.main.state.UiMeal
-import com.practice.main.state.UiMemos
-import com.practice.main.state.UiSchedules
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 
@@ -103,39 +94,10 @@ fun HorizontalCalendarMainScreen(
 @Preview(showBackground = true, device = "spec:width=1280dp,height=800dp,dpi=480")
 @Composable
 private fun HorizontalCalendarMainScreenPreview() {
-    val year = 2022
-    val month = 10
-    val selectedDate = Date(2022, 10, 11)
+    val selectedDate = Date.now()
+    val (year, month, _) = selectedDate
 
-    val uiState = MainUiState(
-        userId = "",
-        year = year,
-        month = month,
-        selectedDate = selectedDate,
-        monthlyDataState = (1..3).map {
-            DailyData(
-                schoolCode = 1,
-                date = Date(2022, 10, 11).plusDays(it),
-                uiMeal = UiMeal(2022, 10, 11, previewMenus, previewNutrients),
-                uiSchedules = UiSchedules(
-                    date = selectedDate,
-                    uiSchedules = previewSchedules,
-                ),
-                uiMemos = UiMemos(
-                    date = selectedDate,
-                    memos = previewMemos,
-                ),
-            )
-        },
-        isLoading = false,
-        selectedSchool = School(
-            name = "어떤 학교",
-            schoolCode = -1,
-        ),
-        isNutrientPopupVisible = false,
-        isMemoPopupVisible = false,
-        mainUiMode = MainUiMode.CALENDAR,
-    )
+    val uiState = previewMainUiState()
     val calendarState = rememberCalendarState(
         year = year,
         month = month,
