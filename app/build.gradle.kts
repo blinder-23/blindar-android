5d420667febcb583b1e3e9dd0c435ffc5bc5d777
Wooyoung Myung <mwy3055@gmail.com>
2024-01-09 18:32:41 +0900

    release: 2.3.0 출시

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "2.2.1"
+        versionName = "2.3.0"
         signingConfig = signingConfigs.getByName("debug")
 
         testInstrumentationRunner = "com.practice.hanbitlunch.HiltTestRunner"
