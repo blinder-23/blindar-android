7c396a32eb870d6ec75a50a06edb11e4992e4868
Wooyoung Myung <mwy3055@gmail.com>
2024-01-09 15:18:59 +0900

    fix: DatePickerCard에 elevation이 적용되지 않던 문제 해결

--- feature/main/src/main/java/com/practice/main/daily/HorizontalDailyMainScreen.kt
+++ feature/main/src/main/java/com/practice/main/daily/HorizontalDailyMainScreen.kt
@@ -9,14 +9,12 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -92,7 +90,6 @@ private fun HorizontalDailyMainScreenContents(
             datePickerState = datePickerState,
             modifier = Modifier
                 .weight(1f)
-                .clip(RoundedCornerShape(16.dp))
                 .fillMaxHeight()
         )
         MainScreenContents(
@@ -113,11 +110,7 @@ private fun DatePickerCard(
 ) {
     ElevatedCard(
         modifier = modifier,
-        colors = CardDefaults.elevatedCardColors(
-            containerColor = MaterialTheme.colorScheme.surface,
-            contentColor = MaterialTheme.colorScheme.onSurface,
-        ),
-        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 4.dp),
+        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 2.dp),
     ) {
         LazyColumn(
             modifier = Modifier.fillMaxHeight(),
