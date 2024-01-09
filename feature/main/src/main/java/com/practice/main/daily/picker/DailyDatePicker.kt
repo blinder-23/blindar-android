1768d46440cdddab847d3de84078118afc11ab1a
Wooyoung Myung <mwy3055@gmail.com>
2024-01-09 15:56:26 +0900

    feature: DailyDatePicker의 스크린 리더 사용성 최적화

--- feature/main/src/main/java/com/practice/main/daily/picker/DailyDatePicker.kt
+++ feature/main/src/main/java/com/practice/main/daily/picker/DailyDatePicker.kt
@@ -19,6 +19,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -45,10 +48,18 @@ fun DailyDatePicker(
 }
 
 @Composable
-private fun DailyDatePickerHeader(selectedDate: Date) {
-    LabelLarge(text = stringResource(id = R.string.daily_date_picker_title))
-    Spacer(modifier = Modifier.height(4.dp))
-    HeadlineLarge(text = selectedDate.toHeadlineFormat())
+private fun DailyDatePickerHeader(
+    selectedDate: Date,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.semantics(mergeDescendants = true) {},
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        LabelLarge(text = stringResource(id = R.string.daily_date_picker_title))
+        Spacer(modifier = Modifier.height(4.dp))
+        HeadlineLarge(text = selectedDate.toHeadlineFormat())
+    }
 }
 
 @Composable
@@ -92,7 +103,9 @@ private fun DailyDatePickerSupportingText(
     LabelMedium(
         text = errorText,
         textColor = errorTextColor,
-        modifier = modifier,
+        modifier = modifier.clearAndSetSemantics {
+            contentDescription = if (isError) errorText else ""
+        },
     )
 }
 
