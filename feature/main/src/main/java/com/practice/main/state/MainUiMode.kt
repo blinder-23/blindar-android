0f55dab0c1f79b3a73ce33eb977ec259b810a8f1
Wooyoung Myung <mwy3055@gmail.com>
2024-01-09 16:49:38 +0900

    feature: MainUiMode에 NOT_SET 값 추가

--- feature/main/src/main/java/com/practice/main/state/MainUiMode.kt
+++ feature/main/src/main/java/com/practice/main/state/MainUiMode.kt
@@ -4,11 +4,13 @@ import com.practice.preferences.preferences.MainScreenMode
 
 enum class MainUiMode {
     LOADING,
+    NOT_SET,
     CALENDAR,
     DAILY,
 }
 
 fun MainScreenMode.toUiMode() = when (this) {
+    MainScreenMode.NOT_SELECTED -> MainUiMode.NOT_SET
     MainScreenMode.Calendar -> MainUiMode.CALENDAR
     MainScreenMode.Daily -> MainUiMode.DAILY
 }
