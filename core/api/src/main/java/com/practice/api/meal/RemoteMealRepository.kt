e0e476e0e4e09306ffe43215842e079ef7ab6cfa
Wooyoung Myung <mwy3055@gmail.com>
2024-01-13 22:40:30 +0900

    refactor: RemoteMealRepository의 로그 제거
    FetchRemoteMealWorker와 함께 너무 많은 로그를 찍던 RemoteMealRepository의 로그를 제거했다.

--- core/api/src/main/java/com/practice/api/meal/RemoteMealRepository.kt
+++ core/api/src/main/java/com/practice/api/meal/RemoteMealRepository.kt
@@ -1,16 +1,12 @@
 package com.practice.api.meal
 
-import android.util.Log
 import com.practice.api.meal.pojo.toMonthlyMeal
 import com.practice.domain.meal.MonthlyMeal
 
 class RemoteMealRepository(private val remote: RemoteMealDataSource) {
     suspend fun getMeals(schoolCode: Int, year: Int, month: Int): MonthlyMeal {
         return try {
-            remote.getMeals(schoolCode, year, month).let {
-                Log.d("RemoteMealRepository", "meal $year $month: ${it.response.size}")
-                it.toMonthlyMeal(schoolCode, year, month)
-            }
+            remote.getMeals(schoolCode, year, month).toMonthlyMeal(schoolCode, year, month)
         } catch (e: Exception) {
             throw RemoteMealRepositoryException(e.message)
         }
