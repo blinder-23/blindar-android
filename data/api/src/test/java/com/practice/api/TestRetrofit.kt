305ab1ae6220cc0398dc5d09e7c46a8f5f693626
Wooyoung Myung <mwy3055@gmail.com>
2024-05-07 21:31:21 +0900

    test: 테스트 TODO 업데이트

--- data/api/src/test/java/com/practice/api/TestRetrofit.kt
+++ data/api/src/test/java/com/practice/api/TestRetrofit.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object TestRetrofit {
-    // TODO: 테스트 코드에 있는 retrofit을 전부 이걸로 바꾸기
+    // TODO: 향후 테스트 서버 생기면, API 테스트 코드에 있는 retrofit을 전부 이걸로 바꾸기
     fun <T> getRetrofit(
         clazz: Class<T>,
         factory: Converter.Factory = GsonConverterFactory.create(),
