35cf59a78014aa074b74b0f1d5fdb9b5a0e0d78b
Wooyoung Myung <mwy3055@gmail.com>
2024-06-13 14:22:43 +0900

    docs: BlindarDialog에 주석 추가

--- core/designsystem/src/main/java/com/practice/designsystem/components/BlindarPopup.kt
+++ core/designsystem/src/main/java/com/practice/designsystem/components/BlindarPopup.kt
@@ -6,6 +6,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.DialogProperties
 
+/**
+ * Modal popup of the blindar app.
+ *
+ * This composable automatically shows opaque background.
+ * [content] will be shown at the center of the screen.
+ *
+ * @param onDismissRequest called when the user clicks outside the dialog or on the back
+ * @param modifier modifiers to set to the dialog container
+ * @param properties properties to set to the dialog
+ * @param content composable which will be shown as a dialog content
+ */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BlindarDialog(
