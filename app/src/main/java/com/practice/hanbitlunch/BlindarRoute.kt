859a17576493a39940f2123452cb0ea5101938b6
Wooyoung Myung <mwy3055@gmail.com>
2024-06-21 21:33:36 +0900

    feature: BlindarRoute를 인터페이스로 수정

--- app/src/main/java/com/practice/hanbitlunch/BlindarRoute.kt
+++ app/src/main/java/com/practice/hanbitlunch/BlindarRoute.kt
@@ -1,60 +1,74 @@
 package com.practice.hanbitlunch
 
+import android.os.Bundle
 import androidx.navigation.NavBackStackEntry
+import androidx.navigation.serialization.decodeArguments
+import com.practice.main.nutrient.NutrientRoute
+import kotlinx.collections.immutable.toPersistentHashMap
+import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.serializer
 
 @Serializable
-sealed class BlindarRoute(private val order: Int) : Comparable<BlindarRoute> {
-    internal val route: String
-        get() = this::class.java.canonicalName?.toString().toString()
-
-    override fun compareTo(other: BlindarRoute): Int {
-        return this.order.compareTo(other.order)
-    }
+sealed interface BlindarRoute {
+    @Serializable
+    data object Splash : BlindarRoute
 
     @Serializable
-    data object Splash : BlindarRoute(0)
+    data object Onboarding : BlindarRoute
 
     @Serializable
-    data object Onboarding : BlindarRoute(1)
+    data object VerifyPhone : BlindarRoute
 
     @Serializable
-    data object VerifyPhone : BlindarRoute(2)
+    data object VerifyUsername : BlindarRoute
 
     @Serializable
-    data object VerifyUsername : BlindarRoute(3)
+    data object SelectSchool : BlindarRoute
 
     @Serializable
-    data object SelectSchool : BlindarRoute(4)
+    data object Main : BlindarRoute
 
     @Serializable
-    data object Main : BlindarRoute(5)
+    data object Settings : BlindarRoute
 
     @Serializable
-    data object Settings : BlindarRoute(6)
+    data class Nutrient(
+        override val year: Int,
+        override val month: Int,
+        override val dayOfMonth: Int,
+        override val schoolCode: Int,
+        override val mealTime: String,
+    ) : BlindarRoute, NutrientRoute
 
     // TODO: add memo, schedule routes
-
-    companion object {
-        // TODO: 왜 Splash만 null로 들어감?
-        private val entries =
-            listOf(Splash, Onboarding, VerifyPhone, VerifyUsername, SelectSchool, Main, Settings)
-
-//        fun NavBackStackEntry.toBlindarRoute() =
-//            entries.firstOrNull {
-//                Log.d("BlindarRoute", "entries: $entries")
-//                Log.d("BlindarRoute", "entry: ${it == null}, this: $this")
-//                it.route == this.destination.route.orEmpty()
-//            } ?: throw IllegalArgumentException("Unknown route: ${this.destination.route}")
-
-        fun NavBackStackEntry.toBlindarRoute(): BlindarRoute = when (this.destination.route) {
-            Splash.route -> Splash
-            Onboarding.route -> Onboarding
-            VerifyPhone.route -> VerifyPhone
-            VerifyUsername.route -> VerifyUsername
-            SelectSchool.route -> SelectSchool
-            Main.route -> Main
-            else -> throw IllegalArgumentException("Unknown route: ${this.destination.route}")
-        }
-    }
 }
+
+private val blindarRoutes = listOf(
+    BlindarRoute.Splash::class,
+    BlindarRoute.Onboarding::class,
+    BlindarRoute.VerifyPhone::class,
+    BlindarRoute.VerifyUsername::class,
+    BlindarRoute.SelectSchool::class,
+    BlindarRoute.Main::class,
+    BlindarRoute.Settings::class,
+    BlindarRoute.Nutrient::class,
+).associateBy {
+    it.simpleName
+}.toPersistentHashMap()
+
+@OptIn(InternalSerializationApi::class)
+fun NavBackStackEntry.toBlindarRoute(): BlindarRoute {
+    val clazz = blindarRoutes[destinationClassName]
+        ?: throw IllegalArgumentException("No route with name: $destinationClassPackageName")
+
+    val bundle = arguments ?: Bundle()
+    val typeMap = destination.arguments.mapValues { it.value.type }
+    return clazz.serializer().decodeArguments(bundle, typeMap)
+}
+
+private val NavBackStackEntry.destinationClassPackageName: String?
+    get() = this.destination.route?.substringBefore("/")
+
+private val NavBackStackEntry.destinationClassName: String
+    get() = destinationClassPackageName?.substringAfterLast(".").orEmpty()
