cb3d180e163813a9b2fb663457d06d19fe2f79af
Wooyoung Myung <mwy3055@gmail.com>
2024-07-21 20:46:03 +0900

    feature: string resource id로 토스트 메시지를 만드는 확장 함수 추가

--- core/util/src/main/java/com/practice/util/ToastUtil.kt
+++ core/util/src/main/java/com/practice/util/ToastUtil.kt
@@ -2,8 +2,12 @@ package com.practice.util
 
 import android.content.Context
 import android.widget.Toast
+import androidx.annotation.StringRes
 
-// TODO: StringRes만 있어도 만들 수 있게 수정
 fun Context.makeToast(message: String, duration: Int = Toast.LENGTH_LONG) {
     Toast.makeText(this, message, duration).show()
+}
+
+fun Context.makeToast(@StringRes stringId: Int, duration: Int = Toast.LENGTH_LONG) {
+    makeToast(getString(stringId), duration)
 }
