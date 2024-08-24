a98b78e3e9e6a9d9a1f0f75f7dee1122ddfaf0e7
Wooyoung Myung <mwy3055@gmail.com>
2024-08-24 16:49:22 +0900

    delete: 사용하지 않는 proguard-rules 파일 제거

--- feature/feedback/build.gradle.kts
+++ feature/feedback/build.gradle.kts
@@ -21,10 +21,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
         }
     }
     compileOptions {
