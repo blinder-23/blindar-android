d1ce7abc828dc322d62760c96034e080ae61646b
Wooyoung Myung <mwy3055@gmail.com>
2024-08-24 16:22:16 +0900

    gradle: 사용자 이름을 입력받는 화면 모듈 추가

---
+++ feature/username/build.gradle.kts
@@ -0,0 +1,77 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("dagger.hilt.android.plugin")
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "com.practice.username"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeCompiler {
+        enableStrongSkippingMode = true
+        includeSourceInformation = true
+    }
+}
+
+dependencies {
+    implementation(project(":core:designsystem"))
+
+    // KTX libraries
+    implementation(libs.androidx.core.ktx)
+
+    // Compose
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.bundles.compose)
+
+    // Accompanist
+    implementation(libs.bundles.accompanist)
+
+    // Firebase
+    implementation(libs.firebase.ui.auth)
+
+    // AndroidX lifecycles
+    implementation(libs.bundles.lifecycle)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    ksp(libs.hilt.compiler.androidx)
+    androidTestImplementation(libs.hilt.android.testing)
+    implementation(libs.hilt.work)
+
+    // Kotlin Coroutines
+    implementation(libs.bundles.coroutines)
+    testImplementation(libs.kotlinx.coroutines.test)
+}
