fab315cd97807df6cfd7fea5f1e6f50244feb4ce
Wooyoung Myung <mwy3055@gmail.com>
2025-08-31 14:24:58 +0900

    test: remove unused tests

--- data/api/src/test/java/com/practice/api/school/SchoolApiTest.kt
+++ data/api/src/test/java/com/practice/api/school/SchoolApiTest.kt
@@ -1,19 +1,16 @@
 package com.practice.api.school
 
 import com.practice.api.school.api.schoolApi
-import kotlinx.coroutines.test.runTest
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 
 class SchoolApiTest {
     private val api = schoolApi
 
-    @Test
-    fun testSchoolApi() = runTest {
-        val response = api.getSupportedSchools()
-        println(response)
-        assertThat(response.isSuccess).isTrue()
-        assertThat(response.response)
-            .isNotEmpty
-    }
+//    @Test
+//    fun testSchoolApi() = runTest {
+//        val response = api.getSupportedSchools()
+//        println(response)
+//        assertThat(response.isSuccess).isTrue()
+//        assertThat(response.response)
+//            .isNotEmpty
+//    }
 }
